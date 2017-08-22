a73f50461daf840cf571202490e39adb9e0d108a
DangitBen <52bandrew@gmail.com>
2017-08-22 14:47:26 -0700

    Fixed some parser methods

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ for file in os.listdir("Profiles"):
     print("Name: ", profile.get_name())
     print("URL Name: ", profile.get_profile_url())
     print("Skills: ", profile.get_skills(), len(profile.get_skills()))
-
+    print("Current Company: ", profile.get_current_company())
     print("Companies: ", profile.get_all_companies())
-<<<<<<< HEAD
+    print("Location: ", profile.get_location())
+    print("Connections : ", profile.get_connection_number())
+
     # print("Bio: ", profile.get_bio())
     # print("Certifications: ", profile.get_certification())
     # print("Current Company: ", profile.get_current_company())
@@ -29,19 +31,6 @@ for file in os.listdir("Profiles"):
     # print("Projects: ", profile.get_projects())
     # print("Connection #: ", profile.get_connection_number())
     # print("Media #: ", profile.get_media_number())
-=======
-    print("Bio: ", profile.get_bio())
-    print("Certifications: ", profile.get_certification())
-    print("Current Company: ", profile.get_current_company())
-    print("Languages: ", profile.get_languages())
-    print("Location: ", profile.get_location())
-    print("Skills: ", profile.get_skills())
-    print("Projects: ", profile.get_projects())
-    print("Honors & Awards: ", profile.get_awards())
-    print("Organizations: ", profile.get_organizations())
-    print("Courses: ", profile.get_courses())
-    print("Connection #: ", profile.get_connection_number())
-    print("Media #: ", profile.get_media_number())
->>>>>>> a80f7424e0eb53c64af2d3cb3123278fcc49e39d
+
 
     print("\n\n")
